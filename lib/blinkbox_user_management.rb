a2247c05a8279fe0d375688c55761a25a5f7da9b
alexjo <alexjo@blinkbox.com>
2014-07-17 17:16:43 +0100

    working to debug delete

--- lib/blinkbox_user_management.rb
+++ lib/blinkbox_user_management.rb
@@ -3,14 +3,24 @@ require 'multi_json'
 require 'net/http/capture'
 module UserManagement
 	class User
+		include HTTParty
+		def initialize(server_uri, email, password)
+			self.class.base_uri(server_uri.to_s)
+			@headers = {}
+			params = {
+				"grant_type" => "password",
+				"username" => email,
+				"password" => password
+			}
+			http_send :post, "/oauth2/token",params
+			@attributes = MultiJson.load(HttpCapture::RESPONSES.last.body)
+		end
 		def has_attribute? key
 			@attributes.has_key? key
 		end
 		def get_token
+			raise "No token found" if !has_attribute? 'access_token'
 			@attributes['access_token']
-		end
-		def initialize(json_blob)
-			@attributes = json_blob
 		end
 		def [](y)
 			if has_attribute? y
@@ -19,38 +29,25 @@ module UserManagement
 				return nil
 			end
 		end
-	end
-
-
-	class UserManager
-		include HTTParty
-		def initialize server_uri, proxy_uri = nill
-			self.class.base_uri(server_uri.to_s)
-			self.class.http_proxy(proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password) if proxy_uri
-			@headers = {}
-		end
-		def get_user email, password
-			params = {
-				"grant_type" => "password",
-				"username" => email,
-				"password" => password
-			}
-			http_send :post, "/oauth2/token",params
-			return !HttpCapture::RESPONSES.last.body.empty? ? User.new(MultiJson.load(HttpCapture::RESPONSES.last.body)) : nil
-		end
-		def get_clients user
-			http_send :get, "/clients",{},user.get_token
+		def get_clients
+			http_send :get, "/clients",{},get_token
 			MultiJson.load(HttpCapture::RESPONSES.last.body)["clients"]
 		end
-		def delete_client user, id
-			http_send :delete, "/clients/#{id}",{},user.get_token
+		def delete_client uri
+			http_call(:delete,uri,{},get_token)	
 		end
-		def delete_all_clients user
-			get_clients(user).each do | client|
-			delete_client user, client["client_id"]
+		def delete_all_clients 
+			get_clients.each do | client|
+			delete_client client["client_uri"]
 			end
 		end
 		private
+		def http_call(verb, uri, params = {}, access_token = nil)
+			headers = { "Accept" => "application/json" }.merge(@headers)
+			headers["Authorization"] = "Bearer #{access_token}" if access_token
+			self.class.send(verb, uri.to_s, headers: headers, query: params)
+			HttpCapture::RESPONSES.last
+		end
 		def http_send(verb, uri, body_params, access_token = nil)
 			headers = { "Accept" => "application/json", "Content-Type" => "application/x-www-form-urlencoded" }.merge(@headers)
 			headers["Authorization"] = "Bearer #{access_token}" if access_token
@@ -61,3 +58,4 @@ module UserManagement
 		end
 	end
 end
+
