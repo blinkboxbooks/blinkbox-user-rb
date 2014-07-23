6a16d80a05396068338698e413a265d2f8b56ec9
alexjo <alexjo@blinkbox.com>
2014-07-23 14:31:51 +0100

    adding last response format option

--- lib/blinkbox-user.rb
+++ lib/blinkbox-user.rb
@@ -9,6 +9,7 @@ module Blinkbox
         def initialize params
           @server_uri = params[:server_uri] || "https://auth.blinkboxbooks.com"
           @proxy_uri = params[:proxy_uri] || nil
+          @http_auth = params[:http_method]
         end
       end
     end
@@ -24,7 +25,15 @@ module Blinkbox
       end
       def refresh
         authenticate(@user_credentials)
-        @attributes = MultiJson.load(HttpCapture::RESPONSES.last.body)
+        @attributes = last_response({:format => "json"})
+      end
+      def last_response params={}
+        case params[:format]
+        when "json"
+          MultiJson.load(HttpCapture::RESPONSES.last.body)
+        else
+          HttpCapture::RESPONSES.last
+        end
       end
       def method_missing(name, *args, &blk)
         if args.empty? && blk.nil? && @attributes.has_key?(name.to_s)
