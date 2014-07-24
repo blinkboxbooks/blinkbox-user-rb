495760aeea5bd2aeae04673ade78e46aedc562cf
Muz <mustaqila@blinkbox.com>
2014-07-24 11:55:04 +0100

    Fix up the namespacing to be consistant with other Blinkbox libs/gems

--- spec/blinkbox/user_spec.rb
+++ spec/blinkbox/user_spec.rb
@@ -1,11 +1,11 @@
-require 'spec_helper'
+require_relative '../spec_helper'
 
 SERVER_URI='https://auth.blinkboxbooks.com'
 USERNAME='acceptancetestingbbb@gmail.com'
 PASSWORD='9e6fdf08-2556-4e9e-b30a-20d97b0e3d19'
 
-describe BlinkboxUser do
-	before do
-		@user = BlinkboxUser.new(SERVER_URI,nil)
-	end
+describe Blinkbox::User do
+  before :each do
+    @user = BlinkboxUser.new(SERVER_URI,nil)
+  end
 end
