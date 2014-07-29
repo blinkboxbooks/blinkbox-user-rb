3744006da5d2394ddab96d59de51de8fbe308f30
alexjo <alexjo@blinkbox.com>
2014-07-29 15:15:56 +0100

    Repaired indentation and following rubocop/lint suggestions

--- lib/blinkbox/user.rb
+++ lib/blinkbox/user.rb
@@ -1,59 +1,56 @@
 require_relative 'zuulclient'
-
 module Blinkbox
-	class Settings
-		attr_reader :server_uri, :proxy_uri
+  class Settings
+    attr_reader :server_uri, :proxy_uri
 
-		def initialize(params)
-			@server_uri = params[:server_uri] || "https://auth.blinkboxbooks.com"
-			@proxy_uri = params[:proxy_uri] || nil
-		end
-	end
+    def initialize(params)
+      @server_uri = params[:server_uri] || "https://auth.blinkboxbooks.com"
+      @proxy_uri = params[:proxy_uri] || nil
+    end
+  end
 
-	def client_settings(params = {})
-		@configuration ||= Settings.new(params)
-	end
+  def client_settings(params = {})
+    @configuration ||= Settings.new(params)
+  end
 end
 
 module Blinkbox
-	class User
+  class User
+    def initialize(params, client = ZuulClient)
+      @user_credentials = params
 
-		def initialize (params, client = ZuulClient)
-			@user_credentials = params
+      @client = client.new(Settings.client_settings.server_uri,
+                           Settings.client_settings.proxy_uri)
 
-			@client = client.new(Settings.client_settings.server_uri,
-			Settings.client_settings.proxy_uri)
+      @client.authenticate(@user_credentials)
+      res  = @client.last_response(:format => "json")
 
-			@client.authenticate(@user_credentials)
-			res  = @client.last_response({:format => "json"})
+      res.keys.each do | key |
+        instance_eval %Q{
+                                        @#{key} = "#{res[key]}"
+                User.class_eval{attr_reader :#{key} }
+      }
+      end
+    end
 
-			res.keys.each do | key |
-				instance_eval %Q{
-					@#{key} = "#{res[key]}"
-					User.class_eval{attr_reader :#{key} }
-				}
-			end
+    def get_clients
+      @client.get_clients_info @access_token
+      @client.last_response(:format => "json")['clients']
+    end
 
-		end
+    def register_client(params)
+      @client.register_client(params, @access_token)
+    end
 
-		def get_clients
-			@client.get_clients_info @access_token
-			@client.last_response({:format => "json"})['clients']
-		end
+    def deregister_client(client_id)
+      @client.deregister_client(client_id, @access_token)
+    end
 
-		def register_client params
-			@client.register_client(params,@access_token)
-		end
-
-		def deregister_client client_id
-			@client.deregister_client(client_id, @access_token)
-		end
-
-		def deregister_client_all
-			get_clients.each do | client |
-				localid = client['client_id'].split(":").last
-				deregister_client(localid)
-			end
-		end
-	end
+    def deregister_client_all
+      get_clients.each do | client |
+        localid = client['client_id'].split(":").last
+        deregister_client(localid)
+      end
+    end
+  end
 end
