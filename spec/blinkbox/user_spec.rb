7440d5360353211e1bd615114da78f734d8618e8
alexjo <alexjo@blinkbox.com>
2014-07-30 13:58:50 +0100

    tidying up last infraction in user_spec

--- spec/blinkbox/user_spec.rb
+++ spec/blinkbox/user_spec.rb
@@ -2,8 +2,7 @@ require_relative '../spec_helper'
 require_relative '../../lib/blinkbox/user'
 include Blinkbox
 
-TESTDATA = { 
-    :access_token => "eyJraWQiOiJibGlua2JveC9wbGF0L2VuYy9yc2EvMSIsImN0eSI6IkpXVCIsImVuYyI6IkExMjhHQ0 \
+TESTDATA = {  :access_token => "eyJraWQiOiJibGlua2JveC9wbGF0L2VuYy9yc2EvMSIsImN0eSI6IkpXVCIsImVuYyI6IkExMjhHQ0 \
         0iLCJhbGciOiJSU0EtT0FFUCJ9.RDGUMvxMG44WKImdMAFFWypaKCmFOCwCqDq24yJobqOjrmfR7V4ZErJTO-xWueqQX5SxzgRui \
         ARNVch5SdfWZZZqkAVupeiQ7_dvffeEdEGm8RtGiCs9f37s9TNayCQOgWiC4aGEQ2JQ20rt2pLF9SkXmuTqLgVRQCZdrAwtzC90G \
         SVRhAI39nIzsBnxhYxGsv12N9-89g1F6q8NTtih19A3jWBKIhFRp4dC21T1vQDw-k6hiPDqbo9G8msC9-U9aoXZToh9y2YjPxAus \
@@ -12,14 +11,14 @@ TESTDATA = {
         UrdmJMSxLkM7Ehh9Z1n254wv-zUS8vJ8Dr-e3DwMIKA81OhoGJtNAg6cphGFFFfPmaEOfeP1oHp-SuDYCi6CJSrJRM-TO4HvJqbX \
         QaHqGSdLAvkTe_fGpDofwpGqEUV7bf7gY_U7cvB-5l3gYmkLFs8Ic-xI6a8lhISXrJLup_Pr5pfHTKuRPdYBWY187K_-lDeF0zJA \
         XOxnfdUhGcrxEM.ObXF30ih4PvbRqv_qzoYdQ",
-    :token_type => "bearer",
-    :expires_in => 1800,
-    :refresh_token => "kWLx_RPKbZUYjH8bk8gHm3LKpkuR2MfMXy25ATmv2pc",
-    :user_id => "urn:blinkbox:zuul:user:10921",
-    :user_uri => "/users/10921",
-    :user_username => "calabash_test@gmail.com",
-    :user_first_name => "Firstname",
-    :user_last_name => "Lastname"
+              :token_type => "bearer",
+              :expires_in => 1800,
+              :refresh_token => "kWLx_RPKbZUYjH8bk8gHm3LKpkuR2MfMXy25ATmv2pc",
+              :user_id => "urn:blinkbox:zuul:user:10921",
+              :user_uri => "/users/10921",
+              :user_username => "calabash_test@gmail.com",
+              :user_first_name => "Firstname",
+              :user_last_name => "Lastname"
 }
 
 class MockClient
